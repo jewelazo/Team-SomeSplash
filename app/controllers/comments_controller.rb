2381de6797026659222b7defa27be0d79747c044
Christian <christianmr268@gmail>
2021-08-13 05:53:09 +0000

    Added comments controllers for categories

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,35 @@
 class CommentsController < ApplicationController
+  # GET /comments/:id
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  # GET /comments/new?category_id="1"
+  def new
+    @comment = Comment.new(category_id: params[:category_id])
+  end
+
+  # POST /employees
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      redirect_to @comment
+    else
+      render :new
+    end
+  end
+
+  # DELETE /comments/:id
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to categories_path(@comment.category)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
