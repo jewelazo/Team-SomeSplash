289422f7b646a488e54a2a18ba617d60886163d6
jewelazo <gpalominoe@uni.pe>
2021-08-13 08:08:34 +0000

    fix comments_count from photos table

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,22 +1,15 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
 
-  # after_create :photos_comments_count
-  # def photos_comments_count
-  #   self.photos.comments_count += 1
-  #   self.photos.save
-  # end
+  after_create do |_comment|
+    photo = Photo.find(commentable_id)
+    photo.comments_count += 1 if commentable_type == 'Photo'
+    photo.save
+  end
+
+  after_destroy do |_comment|
+    photo = Photo.find(commentable_id)
+    photo.comments_count -= 1 if commentable_type == 'Photo'
+    photo.save
+  end
 end
-# after_create do |_song|
-#   album = Album.find(album_id)
-#   album.duration += duration
-#   album.save
-# end
-# def increment_comments_count
-# self.photo.comments_count +=1
-# self.photo.save
-# after_create do
-#   photo = Photo.find(photo_id)
-#   photo.comments_count +=1
-#   photo.save
-# end
