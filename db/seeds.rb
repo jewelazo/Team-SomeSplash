14e815d59605dc01c99fc1dfd2fd7e4796b34569
Anitazc13 <anamaria.zc.13@gmail.com>
2021-08-13 23:49:34 +0000

    Add the seeds for photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,34 @@
 require "json"
 
-data_categories = JSON.parse(File.read("data/categories.json"), symbolize_names: true)
+all_categories = JSON.parse(File.read("data/categories.json"), symbolize_names: true)
+all_photos = JSON.parse(File.read("data/photos.json"), symbolize_names: true)
 
-puts "Start seeding"
+puts "Start initialize the seed "
+Photo.destroy_all
 Category.destroy_all
 
 puts "Seeding categories"
-data_categories.each do |category|
+all_categories.each do |category|
   category_data = category.slice(:name, :description)
   new_category = Category.new(category_data)
   io_path = category[:cover][:io_path]
   filename = category[:cover][:filename]
   new_category.cover.attach(io: File.open(io_path), filename: filename)
-  puts "Category not created.\nErrors: #{new_category.errors.full_messages}" unless new_category.save
+  puts "Category not created. Errors: #{new_category.errors.full_messages}" unless new_category.save
 end
 puts "Finish seeding categories"
+
+puts "Seeding photos"
+all_photos.each do |photo|
+  category=Category.find_by(name: photo[:category])
+  data_photo = {
+      title: photo[:title],
+      description: photo[:description],
+      category: category }
+  new_photo = Photo.new(data_photo)
+  io_path = photo[:image][:io_path]
+  filename = photo[:image][:filename]
+  new_photo.image.attach(io: File.open(io_path), filename: filename)
+  puts "Photo not created. Errors: #{new_photo.errors.full_messages}" unless new_photo.save
+end
+puts "Finish seeding photos"
