93fded2d6ed716d221d99803774657567ffa9fd3
Anitazc13 <anamaria.zc.13@gmail.com>
2021-08-14 21:15:27 +0000

    Add the messages for the errors and some validates

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ApplicationRecord
   validates :title, presence: true, uniqueness: true
-
+  validates :image, presence: true
   belongs_to :category, counter_cache: true
   has_many :comments, as: :commentable, dependent: :destroy
   has_one_attached :image
